eee2991376edfff406d9edfd171f2c28e92ddd1e
kefarova-png <kefarova@gmail.com>
2026-03-24 23:14:07 +0400

    Изменено разбиение на функциию. Добавлена обработка вводимых данных для повышения помехоустойчивости и стабильности работы

--- vote_verification.py
+++ vote_verification.py
@@ -1,18 +1,29 @@
-# Функция для ввода данных и проверок
-def input_and_analyze():
-    age = int(input("Введите Ваш возраст "))
-    citizen = input("Являетесь ли Вы гражданином страны? Введите да или нет ")
+# Функция для ввода данных, их проверки и форматирования:
+# проверка ввода числа с повторным запросом при ошибке ввода,
+# удаление возможных пробелов по краям каждого из ответов,
+# перевод букв в нижний регистр,
+# ошибочный ввод ДА и НЕТ не фильтруется (кроме регистра).'
+def input_and_formatting():
+    while True:
+        try:
+            age = int(input("Введите Ваш возраст ").strip())
+            break
+        except ValueError:
+            print("Пожалуйста, введите число для возраста.")
+    citizen = input("Являетесь ли Вы гражданином страны? Введите ДА или НЕТ ").strip().lower()
     disqualification = input("""Есть ли у Вас диквалификация на участие в выборах? 
 (например, по причине уголовного наказания)
-Введите да или нет """)
+Введите ДА или НЕТ """).strip().lower()
+    return age, citizen, disqualification
+
+
+# Функция проверки условий участия в выборах и вывода результата
+def analyzing_and_output(age, citizen, disqualification):
     if age >= 18 and citizen == "да" and disqualification == "нет": # проверка выполнения 3х условий одновременно
-        return 'Вы можете голосовать на выборах.'
-    return 'Вы не можете голосовать на выборах.'
+        print('Вы можете голосовать на выборах.')
+    else:
+        print('Вы не можете голосовать на выборах.')
 
 
-# Функция для вывода результата
-def display_resuls():
-    print(input_and_analyze())
-
-
-display_resuls()
+age, citizen, disqualification = input_and_formatting()
+analyzing_and_output(age, citizen, disqualification)
